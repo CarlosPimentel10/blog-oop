ce9c3a2e49bdc7f82ae5acc5a6a3f36baa6856e3
unknown <carlos.costa.pimentel@gmail.com>
2024-12-26 16:18:11 +0000

    fixed routing bug

--- public/index.php
+++ public/index.php
@@ -3,16 +3,17 @@ declare(strict_types=1);
 
 use Core\Router;
 
-
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../routes.php';
 
-
+// Initialize the Router
 $router = new Router();
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+// Include routes (uses the $router instance)
+require_once __DIR__ . '/../routes.php';
+
+// Get the current URI and request method
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Dispatch the router
 echo $router->dispatch($uri, $method);
-
-?>
--- routes.php
+++ routes.php
@@ -1,21 +1,21 @@
 <?php
 declare(strict_types= 1);
 /**
- * @var Core\Router $router
+ * @var Core\Router $router;
  */
 // 
 
 // TODO:BUG FIX, Project not served on the routes specified
-require_once __DIR__ . '/core/Router.php';
+/* require_once __DIR__ . '/core/Router.php';
 
 use Core\Router;
 
 if(!isset($router)){
     $router = new Router();
-}
+} */
 
-$router->add(method:'GET', uri: '/', controller: 'HomeController@index');
+$router->add('GET', '/', 'HomeController@index');
+$router->add('GET', '/posts', 'PostController@index');
+$router->add('GET', '/posts/{id}', 'PostController@show');
 
-$router->add(method:'GET',uri: '/posts', controller:'PostController@index');
-$router->add(method:'GET', uri:'/posts/{id}', controller:'PostController@show');
-
+// var_dump($router);
