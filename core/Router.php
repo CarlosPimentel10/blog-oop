6b397f195d9b0762bc5d4487caf68b1c0481b9eb
unknown <carlos.costa.pimentel@gmail.com>
2024-12-26 12:32:25 +0000

    project setup routing and scaffolding

---
+++ core/Router.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Core;
+
+class Router
+{
+
+    protected $routes = [];
+
+    public function add(string $method, string $uri, string $controller): void
+    {
+
+    }
+
+    public function dispatch(string $uri, string $method): string {
+        
+    }
+
+    protected function findRoute(string $uri, string $method):?array {
+    
+    }
+
+    protected function matchRoute(string $routeUri, string $requestUri):?array {
+    
+    }
+
+    protected function callAction(string $controller, string $action, array $params):string {
+    
+    }
+}
+
+?>
