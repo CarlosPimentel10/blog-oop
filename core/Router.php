1a6274e9f30fbfbbebde29cf4209f0e99192a675
unknown <carlos.costa.pimentel@gmail.com>
2024-12-26 12:50:01 +0000

    implemented matchRoute

--- core/Router.php
+++ core/Router.php
@@ -12,19 +12,40 @@ class Router
 
     }
 
-    public function dispatch(string $uri, string $method): string {
-        
+    public function dispatch(string $uri, string $method): string
+    {
+
     }
 
-    protected function findRoute(string $uri, string $method):?array {
-    
+    protected function findRoute(string $uri, string $method): ?array
+    {
+
     }
 
-    protected function matchRoute(string $routeUri, string $requestUri):?array {
-    
+    protected function matchRoute(string $routeUri, string $requestUri): ?array
+    {
+        $routeSegments = explode('/', trim($routeUri, '/'));
+        $requestSegments = explode('/', trim($requestUri, '/'));
+
+        if (count($routeSegments) !== count($requestSegments)) {
+            return null;
+        }
+
+        $params = [];
+
+        foreach ($routeSegments as $index => $routeSegment) {
+            if (str_starts_with($routePart, '{') && str_ends_with($routePart, '}') {
+                $params[trim($routePart, '{}')] = $requestSegments[$index];
+            } elseif($routeSegment !== $requestSegments[$index]) {
+                return null;
+            }
+        }
+
+        return $params;
     }
 
-    protected function callAction(string $controller, string $action, array $params):string {
+    protected function callAction(string $controller, string $action, array $params): string
+    {
         $controllerClass = "App\\Controller\\$controller";
         return (new $controllerClass)->$action(...$params);
     }
