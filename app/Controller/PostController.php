07d7c8729e66240fc552be05ec17d63eda3a3a97
unknown <carlos.costa.pimentel@gmail.com>
2024-12-26 13:45:38 +0000

    created controllers

---
+++ app/Controller/PostController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+
+class PostController {
+    public function index() {
+        return "All posts";
+    }
+
+    public function show($id){
+        return "Post nr $id";
+    }
+}
+
+?>
