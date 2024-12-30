04a5e005e33d226b0d43a5191dff5918dc162b11
unknown <carlos.costa.pimentel@gmail.com>
2024-12-30 12:02:19 +0000

    created comment model and schema:fixtures

---
+++ app/Models/Comment.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types= 1);
+
+namespace App\Models;
+
+use core\Model;
+
+
+
+class Comment extends Model {
+    protected static $table = 'comments';
+  }
+
+?>
--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-
 require_once __DIR__ . '/vendor/autoload.php';
 
 $config = require_once __DIR__ . '/config.php';
@@ -10,9 +9,13 @@ use Core\App;
 use Core\Database;
 use Core\ErrorHandler;
 
-set_exception_handler([ErrorHandler::class,'handleException']);
+// Error handling setup
+set_exception_handler([ErrorHandler::class, 'handleException']);
 set_error_handler([ErrorHandler::class, 'handleError']);
 
+// Bind configuration to the App container
 App::bind('config', $config);
-// new Database receives an array
-App::bind('database', new Database(array($config['database'])));
+
+// Pass only the 'database' part of the configuration
+App::bind('database', new Database($config['database']));
+var_dump($config);
